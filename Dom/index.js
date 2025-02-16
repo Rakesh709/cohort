57152221736e4ea060b3a421c6b0f0b8397a3c1a
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-02-17 00:11:39 +0530

    cohort code

---
+++ Dom/index.js
@@ -0,0 +1,14 @@
+Function.prototype.describe= function(){
+    console.log(`Function name is ${this.name}`);
+    
+}
+
+function greet(name){
+    return `Hello ${name}`
+}
+
+let p1= greet("Rakesh")
+//console.log(p1);
+//console.log(p1.describe);
+
+
