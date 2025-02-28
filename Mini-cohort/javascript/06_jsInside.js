aaa1edf59976a7306cba0b1ad281f60653218118
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-03-01 01:19:07 +0530

    behind the seen minicohort

--- Mini-cohort/javascript/06_jsInside.js
+++ Mini-cohort/javascript/06_jsInside.js
@@ -17,3 +17,35 @@ setTimeout(obj.greet.bind(obj), 2*1000);
 console.log("Bye Bye");
 
 
+// what is starVation ?
+
+//ager micro task ko khali na hone do to startvation ho jata hai
+
+
+Promise.resolve().then(()=>{
+    console.log("1. Promise resolve ho gya");
+    
+    Promise.resolve().then(()=>{
+        console.log("2. Promise resolve ho gya");
+        
+        Promise.resolve().then(()=>{
+            console.log("3. Promise resolve ho gya");
+            
+        })
+    })
+})
+
+//https://www.jsv9000.app/ 
+
+// HLD : HIGH LEVEL DESIGN --> UPPER UPPER SAI 
+// LWD : LOW LEVEL DESIGN --> IN DEPTH
+
+// KUCH KAI EVENT LOOP 
+
+// KUCH KAI PROMISES --> POLYFILLS 
+
+
+//Hoisting 
+
+//let and const is hoisting but temperoral dead zone
+// var --> hoisting hoti hai 
