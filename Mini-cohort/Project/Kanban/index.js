d209fe89d871cb40fd806b6ce9f812cd04178079
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-03-04 23:00:59 +0530

    kanban board

--- Mini-cohort/Project/Kanban/index.js
+++ Mini-cohort/Project/Kanban/index.js
@@ -1,6 +1,20 @@
+const { ServerMonitoringMode } = require("mongodb");
+
 const addTaskBtn = document.getElementById("add-task-btn")
 
 const todoBoard = document.getElementById("todo-board")
+
+
+function attachDragEvent(target){
+    target.addEventListener("dragstart",()=>{
+        target.classList.add("flying");
+    })
+    target.addEventListener("dragend",()=>{
+        target.classList.remove("flying");
+    })
+}
+
+
 
 
 addTaskBtn.addEventListener("click",()=>{
@@ -12,6 +26,8 @@ addTaskBtn.addEventListener("click",()=>{
     taskCard.classList.add("item")
     taskCard.innerText=input
     taskCard.setAttribute("draggable",true)
+    
+    attachDragEvent(taskCard)
 
     todoBoard.appendChild(taskCard)
 
@@ -24,17 +40,52 @@ addTaskBtn.addEventListener("click",()=>{
 
 const allBoard = document.querySelectorAll(".board")
 
-const allItems = document.querySelectorAll(".items")
+const allItems = document.querySelectorAll(".item")
 
-allItems.forEach(item =>{
-    item.addEventListener()
-})
+// allItems.forEach(item =>{
+//     item.addEventListener("dragstart",()=>{
+//         item.classList.add("flying");
+//     })
+//     item.addEventListener("dragend",()=>{
+//         item.classList.remove("flying");
+//     })
+// })
+
+allItems.forEach(item => attachDragEvent(item))
+
+// allItems.forEach( attachDragEvent)
+
 
 
 allBoard.forEach(board =>{
     board.addEventListener('dragover',()=>{
-        console.log(board,"Kuch to gya");
         const flyingElement = document.querySelector(".flying")
+        console.log(board,"Kuch to gya");
+        
+        board.appendChild(flyingElement)
         
     })
 })
+
+
+//card = Drag start
+                    // Board- Drag Over
+//Card - Drag End
+
+
+// drag start --> flyingElement
+// drag end --> flying ko atta do
+
+//featured
+/*
+edit task
+deletecard
+dynamically add baored
+remove board
+drag sorting
+task local storage
+Board head show count of task
+time on every task
+
+
+*/
