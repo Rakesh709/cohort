a5f6645c7d309ef0ab461378640bfa2a165b4aae
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-03-08 22:57:53 +0530

    inheritance

--- Interview-Skill/inheritance.js
+++ Interview-Skill/inheritance.js
@@ -16,8 +16,21 @@ const p2= Object.create(p1)
 
 /*  
  Wrapper classs--> String , Number , Boolean
-
-
-
 */
 
+
+// const fname = new String()
+// fname__proto__= String.prototype
+
+
+const p01= {
+    xp1:"I am inside P1"
+}
+
+const p02= {
+    xp2:"I am inside P2"
+}
+
+const p03= {
+    xp3:"I am inside P3"
+}
