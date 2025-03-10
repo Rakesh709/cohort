71b7ef4670d4a8c09896b4155aa7a29d5c3f0d39
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-03-11 00:12:46 +0530

    model, controller,routes completed

--- FullStack-Tech/index.js
+++ FullStack-Tech/index.js
@@ -3,6 +3,10 @@ import dotenv from "dotenv"
 import cors from "cors"
 import db from "./utils/db.js";
 
+
+//import all routes
+import userRoutes from "./routes/user.routes.js"
+
 dotenv.config();
 //if diffrent folder path mentioned in config
 
@@ -40,6 +44,9 @@ app.get("/rakesh",(req,res)=>{
 
 //connect to db
 db();
+
+//user routes
+app.use("/api/v1/users",userRoutes)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
