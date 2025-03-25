7d9d6a92d34d5d0055cd42562b604a91fdde4a1b
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-03-25 23:43:15 +0530

    server without express

--- Backend/Node/server.js
+++ Backend/Node/server.js
@@ -1,3 +1,22 @@
-const math = require('./math.js');
-const {add} = require('./math.js')
-console.log(math.add(2,5));
+//server without express
+
+const http = require("http");
+
+const server=http.createServer(function(req,res){
+    console.log("Incoming req ayya");
+    res.end('yelo response')
+});
+
+server.listen(8000,function(){
+    console.log("Server started");
+    
+})
+
+const express = require("express");
+const { appendFile } = require("fs");
+
+const handlerFunction = express();
+
+
+handlerFunction.get('/',(req,res)=> res.send("Homepage"))
+
