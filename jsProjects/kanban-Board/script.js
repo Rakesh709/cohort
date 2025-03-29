6fcae51831dfa03536356dea3f5b1e0504ad1d34
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-03-29 19:54:57 +0530

    kunbon create task element

--- jsProjects/kanban-Board/script.js
+++ jsProjects/kanban-Board/script.js
@@ -1,4 +1,26 @@
 
 function addTask(columnId){
     const input =document.getElementById(`${columnId}-input`)
+    const taskText = input.value.trim();
+    console.log(taskText)
+    
+    if(taskText===""){
+        return;
+    }
+
+    const taskElement = createTaskElement(taskText);
+
+    document.getElementById(`${columnId}-task`).appendChild(taskElement);
+    input.value='';
 }
+
+function createTaskElement(taskText){
+    const element = document.createElement("div");
+    element.textContent= taskText;
+    element.classList.add("card");
+    // element.setAttribute("draggable",true)
+    element.draggable="true"
+    element.addEventListener("dragstart",dragStart)
+    return element;
+}
+//1:20 MINT
