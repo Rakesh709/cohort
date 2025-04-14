f6c8864cd42002cb07ef11b91e5a494fd0983dc9
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-04-14 22:21:05 +0530

    signup url

--- FrontEnd-Framework/React/service/apiClient.js
+++ FrontEnd-Framework/React/service/apiClient.js
@@ -1,59 +1,58 @@
-class ApiClient{
-    constructor(){
-        this.baseUrl ="http://localhost:5173/api/v1";
-        this.defaultHeaders={
-            "Content-Type":"application/json",
-            "Accept":"application.json",
-        };
+class ApiClient {
+  constructor() {
+    this.baseUrl = "http://localhost:5173/api/v1";
+    this.defaultHeaders = {
+      "Content-Type": "application/json",
+      "Accept": "application.json",
+    };
+  }
+
+  //custom fetch method
+  async customFetch(endpoint, options = {}) {
+    try {
+      const url = `${this.baseUrl}${endpoint}`;
+      const headers = { ...this.defaultHeaders, ...options.headers };
+
+      const config = {
+        ...options,
+        headers,
+        credentials: "includes"
+      };
+      console.log(`Fetching ${url}`);
+      const response = await fetch(url, config);
+
+      //CHECK IF RESPONSE.OK == 200
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Api Error", error);
+      throw error;
     }
+  }
 
-    //custom fetch method 
-    async customFetch(endpoint,options = {}){
-        try {
-            const url = `${this.baseUrl}${endpoint}`;
-            const headers = {...this.defaultHeaders,...options.headers}
+  //Auth endpoints
 
-            const config = {
-                ...options,
-                headers,
-                credentials:"includes"
-            }
-            console.log(`Fetching ${url}`);
-          const response=   await fetch(url,config)
+  async signup(name, email, password) {
+    return this.customFetch("/users/register", {
+      method: "POST",
+      body: JSON.stringify({ name, email, password }),
+    });
+  }
 
-          //CHECK IF RESPONSE.OK == 200
-        const data= await response.json();
-        return data
+  async login(email, password) {
+    return this.customFetch("/users/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+    });
+  }
 
-        } catch (error) {
-            console.error("Api Error",error)
-            throw error
-        }
-    }
-
-    //Auth endpoints
-
-    async signup(name,email,password){
-       return this.customFetch('/users/register',{
-            method:"POST",
-            body:JSON.stringify({name,email,password})
-        })
-    }
-
-    async login(email,password){
-       return this.customFetch("/users/login",{
-            method:"POST",
-            body:JSON.stringify({email,password})
-        })
-
-    }
-
-    async getProfile(){
-       return this.customFetch("/users/me"); 
-    }
-
-
+  async getProfile() {
+    return this.customFetch("/users/me");
+  }
 }
+
+const apiClient = new ApiClient();
+export default apiClient;
 
 //example for header is when we use iphone some other price compare to android
 //when open something then download owr ios or andrioid app
--- FrontEnd-Framework/React/src/components/Signup.jsx
+++ FrontEnd-Framework/React/src/components/Signup.jsx
@@ -1,75 +1,84 @@
-import React, { useState } from 'react'
-
+import React, { useState } from "react";
+import apiClient from "../../service/apiClient.js";
 function Signup() {
-    const [name, setName] = useState("")
-    const [ email, setEmail] = useState("")
-    const [password, setPassword] = useState("");
-    const [loading, setLoading]= useState(false);
-    const [ error, setError] = useState("")
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
 
-    const handleSubmit = async (e)=>{
-        e.preventDefault()
-        setLoading(true)
+    setError("");
 
-        setError('')
+    // MAKKE A API CALL TO BAACKEND WITH DATA
+    //GET RESPONSE FROM BACKEDN
+    //TAKE ACTION BASED ON RESPONSE
 
-        // MAKKE A API CALL TO BAACKEND WITH DATA
-        //GET RESPONSE FROM BACKEDN
-        //TAKE ACTION BASED ON RESPONSE
+    try {
+      console.log("Trying to do a signup");
+      const data = await apiClient.signup(name, email, password);
+      console.log("Signup response",data);
+      
+    } catch (error) {
+        console.log("error here");
+        
     }
-
+  };
 
   return (
-    <div className=''>
-        <h1>Welcome to signup page</h1>
-        
-        <form onSubmit={handleSubmit}>
-            <div className='form-group'>
-                <label htmlFor="name">Name:</label>
-                <input type="text"
-                name='name'
-                id='name'
-                value={name}
-                onChange={(e)=>setName(e.target.value)}
-                required
-                 />
-            </div>
-            {/* email */}
-            <div className='form-group'>
-                <label htmlFor="email">Email:</label>
-                <input type="email"
-                name='email'
-                id='email'
-                value={email}
-                onChange={(e)=>setEmail(e.target.value)}
-                required
-                 />
-            </div>
+    <div className="">
+      <h1>Welcome to signup page</h1>
 
-            {/* password */}
-            <div className='form-group'>
-                <label htmlFor="password">Password:</label>
-                <input type="password"
-                name='password'
-                id='password'
-                value={password}
-                onChange={(e)=>setPassword(e.target.value)}
-                required
-                 />
-            </div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </div>
+        {/* email */}
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
 
-            {/* conditional rendering  */}
-            {/* { Boolean ? " " : ""} */}
-            {/* {loading ?"signup..": "signup.."} */}
-            <button
-            type='submit'
-            disabled={loading}
-            >{loading ?"signup..": "signup.."}</button>
+        {/* password */}
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
 
-        </form>
+        {/* conditional rendering  */}
+        {/* { Boolean ? " " : ""} */}
+        {/* {loading ?"signup..": "signup.."} */}
+        <button type="submit" disabled={loading}>
+          {loading ? "signup.." : "signup.."}
+        </button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
