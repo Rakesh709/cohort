dd873069063d46573902db588a5826ffe5877090
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-04-14 21:13:05 +0530

    react router setup

---
+++ FrontEnd-Framework/React/src/components/Signup.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react'
+
+function Signup() {
+    const [name, setName] = useState("")
+    const [ email, setEmail] = useState("")
+    const [password, setPassword] = useState("");
+    const [loading, setLoading]= useState(false);
+    const [ error, setError] = useState("")
+
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault()
+        setLoading(true)
+
+        setError('')
+
+        // MAKKE A API CALL TO BAACKEND WITH DATA
+        //GET RESPONSE FROM BACKEDN
+        //TAKE ACTION BASED ON RESPONSE
+    }
+
+
+  return (
+    <div className=''>
+        <h1>Welcome to signup page</h1>
+        
+        <form onSubmit={handleSubmit}>
+            <div className='form-group'>
+                <label htmlFor="name">Name:</label>
+                <input type="text"
+                name='name'
+                id='name'
+                value={name}
+                onChange={(e)=>setName(e.target.value)}
+                required
+                 />
+            </div>
+            {/* email */}
+            <div className='form-group'>
+                <label htmlFor="email">Email:</label>
+                <input type="email"
+                name='email'
+                id='email'
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)}
+                required
+                 />
+            </div>
+
+            {/* password */}
+            <div className='form-group'>
+                <label htmlFor="password">Password:</label>
+                <input type="password"
+                name='password'
+                id='password'
+                value={password}
+                onChange={(e)=>setPassword(e.target.value)}
+                required
+                 />
+            </div>
+
+            {/* conditional rendering  */}
+            {/* { Boolean ? " " : ""} */}
+            {/* {loading ?"signup..": "signup.."} */}
+            <button
+            type='submit'
+            disabled={loading}
+            >{loading ?"signup..": "signup.."}</button>
+
+        </form>
+    </div>
+  )
+}
+
+export default Signup
--- FrontEnd-Framework/React/src/main.jsx
+++ FrontEnd-Framework/React/src/main.jsx
@@ -3,16 +3,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-import { createBrowserRouter,Route,Routes } from "react-router";
+import { BrowserRouter,Route,Routes } from "react-router";
 import Login from "./components/Login.jsx";
+import Signup from "./components/Signup.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <createBrowserRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}/>
         <Route path="/login" element={<Login />}/>
+        <Route path="/signup" element={<Signup />}/>
       </Routes>
-    </createBrowserRouter>
+    </BrowserRouter>
   </StrictMode>
 );
